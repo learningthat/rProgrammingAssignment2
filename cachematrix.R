5c06b344ab2abf635811eff43c3fa7f4daa10ae3
Learning That <learningthat@users.noreply.github.com>
2014-12-15 00:06:13 +0100

    initial
    TODO(learningthat): Fill functions content, amend comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+# My solution to the Coursera course "R Programming", assignment 2:
+# https://www.coursera.org/course/rprog
+# 
+# Trying to adhere to the "Google R Style Guide" for readability:
+# http://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+#
+# Assignment: caching the inverse of a matrix
+# 
+# Functions:
+# makeCacheMatrix: 
+# cacheSolve: 
+#
+# Assumptions: Matrix is always invertible (i.e. no further checks)
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+# This function caches a matrix.
 
 }
 
@@ -11,5 +22,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+# This function 
         ## Return a matrix that is the inverse of 'x'
 }
