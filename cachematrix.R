c3b350be7fe30ea1e80f80ce4b92240ef9196a0b
Learning That <learningthat@users.noreply.github.com>
2014-12-21 16:21:40 +0100

    Final commit for review
    Added function content and descriptions

--- cachematrix.R
+++ cachematrix.R
@@ -6,22 +6,43 @@
 #
 # Assignment: caching the inverse of a matrix
 # 
-# Functions:
-# makeCacheMatrix: 
-# cacheSolve: 
+# Functions used:
+# makeCacheMatrix:
+#   set/get matrix value, set/get inverse matrix value
+# cacheSolve:
+#   calculate inverse matrix if not cached, or retrieve cached inversed matrix
 #
 # Assumptions: Matrix is always invertible (i.e. no further checks)
 
 
 makeCacheMatrix <- function(x = matrix()) {
-# This function caches a matrix.
-
+# This function caches a matrix:
+#   set/get matrix value
+#   set/get inverse matrix value
+  i  <- NULL
+  set  <- function(y){
+    x <<- y
+    i <<- NULL 
+  }
+  get  <- function() x
+  setinverse  <- function(inverse) i  <<- inverse
+  getinverse  <- function() i
+  list(set= set, get = get, 
+    setinverse = setinverse, 
+    getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-# This function 
-        ## Return a matrix that is the inverse of 'x'
+# This function calculates the inverse matrix if it was not calculated and
+# cached before. Otherwise, it will retrieve the cached inversed matrix.
+  i  <- x$getinverse()
+  if (!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data  <- x$get()
+  i  <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
